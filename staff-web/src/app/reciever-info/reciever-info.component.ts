d78b28f728f2c0a0f45e406a85c35bef9181d484
Tharit Wichiankoo <tharit@bonmek.com>
2020-04-10 11:08:07 +0700

    [Fix] mock data

--- staff-web/src/app/reciever-info/reciever-info.component.ts
+++ staff-web/src/app/reciever-info/reciever-info.component.ts
@@ -9,36 +9,13 @@ import { DISEASE } from '../core/diseaseType'
 })
 export class RecieverInfoComponent implements OnInit {
   @Input() set recieverInfo(data) {
-    // this.recieverData = data
-    // this.prepareData(data)
+    this.recieverData = data
+    this.prepareData(data)
   }
 
-  recieverData = {
-    firstname: 'ธนพนธ์',
-    lastname: 'ท่อประสิทธิ์',
-    isUsePassport: false,
-    nationalId: '1100801188111',
-    phoneNumber: '0993023911',
-    homeNumber: '3/186',
-    homeMoo: '2',
-    homePostalCode: '90000',
-    homeSubDistrict: 'เขารูปช้าง',
-    homeDistrict: 'เมือง',
-    homeProvince: 'สงขลา',
-    allergies: ['NO_SEA_FOOD', 'NO_MEAT'],
-    diseases: ['GOUT'],
-    remark: 'ต้องการผ้าปูที่นอนเสื่อหมอนฟูก',
-  }
-  members = [
-    {
-      isUsePassport: 'false',
-      firstname: 'ธนพนธ์2222',
-      lastname: 'ท่อประสิทธิ์222',
-      nationalId: '1100801188111',
-      allergies: ['NO_MEAT', 'NO_CHICKEN', 'NO_SEA_FOOD'],
-      diseases: ['DIABETES', 'HEART_DISEASE'],
-    },
-  ]
+ 
+  recieverData
+  members
   ALLERGIES
   DISEASE
   constructor() {}
