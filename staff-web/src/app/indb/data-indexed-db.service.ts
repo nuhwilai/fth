71063a42718460db037b41ad70fad7a78f0fe26a
suthon <suthon@gmail.com>
2020-04-15 16:39:44 +0700

    list with limit

--- staff-web/src/app/indb/data-indexed-db.service.ts
+++ staff-web/src/app/indb/data-indexed-db.service.ts
@@ -27,29 +27,33 @@ export class DataIndexedDbService {
     return this.db
   }
 
-  async list(storeName): Promise<any[]> {
+  async list(storeName: string, limit = 1000): Promise<any[]> {
     const results = []
+    let count = 0
     let cursor = await this.db.transaction(storeName).store.openCursor()
     while (cursor) {
-      results.push(cursor.value)
-      cursor = await cursor.continue()
+      results.push(cursor.value)      
+      count++
+      if(count > limit)
+        break
+      cursor = await cursor.continue()            
     }
     return results
   }
 
-  async deleteRecord(storeName, key: number): Promise<void> {
+  async deleteRecord(storeName: string, key: number): Promise<void> {
     await this.db.delete(storeName, key)
   }
 
-  async clearStore(storeName): Promise<void> {
+  async clearStore(storeName: string): Promise<void> {
     return this.db.clear(storeName)
   }
 
-  async addToStore(storeName, data): Promise<number> {
+  async addToStore(storeName: string, data): Promise<number> {
     return this.db.put(storeName, data)
   }
 
-  async countRecords(storeName): Promise<number> {
+  async countRecords(storeName: string): Promise<number> {
     return this.db.count(storeName)
   }
 
