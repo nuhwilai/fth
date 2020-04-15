8cdaaa250fc1a2735d1edbe9e8a9260613ad2914
Thanaphon Toprasit <thanaphon@bonmek.com>
2020-04-15 20:01:26 +0700

    [Edit] google spread sheet request data

--- villagers-web/src/environments/environment.prod.ts
+++ villagers-web/src/environments/environment.prod.ts
@@ -3,4 +3,5 @@ const serverUrl = ''
 export const environment = {
   production: false,
   apiUrl: serverUrl + '/api',
+  googleSpreadSheetPublicId: "2PACX-1vQNk_l9mXzfL5BrpeTfKdmyL3wtkvVG-KsLIX0fNNTr9ysIMPTR0infOfCwRzDTTsQ4tO_sTcK81vdI"
 }
