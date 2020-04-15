6c3a9963bf26985d4441be6a547b608e85ce3d1e
suthon <suthon@gmail.com>
2020-04-15 11:55:16 +0700

    fix export

--- staff-web/src/app/indb/syncable-data.service.ts
+++ staff-web/src/app/indb/syncable-data.service.ts
@@ -5,7 +5,7 @@ import * as _ from 'lodash'
 @Injectable({
     providedIn: 'root',
 })
-class SyncableDataService {    
+export class SyncableDataService {    
     db: any
     constructor(
         private dataIndexedDbService: DataIndexedDbService
